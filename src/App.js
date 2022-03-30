40303b81368df6860bc153bbb1a2cbfea1b7bc9e
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2022-03-30 12:14:00 -0400

    Node and Channel classes with channel switching

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-import { Node } from "./network";
+import { Node, Channel } from "./network";
 
 function App() {
+
+  const changeChannel = (node, numChannels) => {
+    const switchVal = Math.random();
+    if ((numChannels - 1)/numChannels > switchVal) {
+        // This changes the channel on the Node, may need to do stuff with Channel class too
+        node.channel = channels[Math.floor(Math.random() * numChannels)];
+    }
+}
+
+  const channels = [];
+  for (let i = 0; i < 10; i++) {
+    channels.push(new Channel(`channel${i}`, []));
+  }
+  
+  const len = channels.length;
+
   const nodes = [];
   for (let i = 0; i < 5; i++) {
-    nodes.push(new Node(`n${i}`, [], i));
+    nodes.push(new Node(`n${i}`, channels[i], []));
   }
+  nodes.forEach(node => changeChannel(node, len));
+
   return (
     <div className="App">
       {nodes.map((node) => {
-        return <div>{node.name}</div>;
+        return <div>{node.name}: {JSON.stringify(node.channel)}</div>;
       })}
     </div>
   );
--- src/network.js
+++ src/network.js
@@ -1,15 +1,25 @@
 export class Node {
-    constructor(name, channel, neighbors) {
-        this.name = name;
-        this.neighbors = neighbors;
-        this.channel = channel;
-    }
+  constructor(name, channel, neighbors) {
+    this.name = name;
+    this.channel = channel;
+    this.neighbors = neighbors;
+  }
+}
 
-    switch(numChannels) {
-        const switchVal = Math.random();
-        if ((numChannels - 1)/numChannels > switchVal) {
-            // This changes the channel on the Node, may need to do stuff with Channel class too
-            this.channel = Math.floor(Math.random() * numChannels);
-        }
+export class Channel {
+  constructor(name, nodes) {
+    this.name = name;
+    this.nodes = nodes;
+  }
+
+  removeNode(node) {
+    const index = this.nodes.indexOf(node);
+    if (index > -1) {
+      this.nodes.splice(index, 1); // 2nd parameter means remove one item only
     }
+  }
+
+  addNode(node) {
+      this.nodes.push(node);
+  }
 }
